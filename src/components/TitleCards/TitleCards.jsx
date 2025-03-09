34101d0959b83966f55c12023a968028c01aac2c
Siddharth Mehta <siddharthmehta@Siddharths-MacBook-Air.local>
2025-03-09 20:00:14 +0530

    Initial commit

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import "./TitleCards.css";
+import { Link } from "react-router-dom";
+
+const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_KEY}`,
+    },
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${
+        category || "now_playing"
+      }?language=en-US&page=1`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className="title-cards">
+      <h2>{title || "Popular on Netflix"}</h2>
+      <div className="card-list">
+        {apiData?.map((card, index) => {
+          return (
+            <Link to={`/player/${card.id}`} className="card" key={index}>
+              <img
+                src={"https://image.tmdb.org/t/p/w500" + card.backdrop_path}
+                alt=""
+              />
+              <p>{card.original_title}</p>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TitleCards;
